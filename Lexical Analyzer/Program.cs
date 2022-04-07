0984ab16b08f5249a7fd98220c037860a0822cc4
livingtwentytwo <nikkithea15@gmail.com>
2022-04-07 12:52:50 +0800

    Add project files.

---
+++ Lexical Analyzer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lexical_Analyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TokenAnalyzer analyzer = new TokenAnalyzer();
+            int inputLength;
+
+            var fileStream = new FileStream(@"C:\Users\USER\Desktop\Everything\test.txt", FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    if (line != String.Empty)
+                    {
+                        string[] inputArray = line.Split(' ');
+                        inputLength = inputArray.Length;
+
+                        for (int i = 0; i < inputLength; i++)
+                        {
+                            if (i == inputLength - 1)
+                            {
+                                string data;
+                                data = inputArray[i].Substring(0, inputArray[i].Length - 1);
+                                analyzer.input_check(data);
+                            }
+                            else
+                            {
+                                analyzer.input_check(inputArray[i]);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input!");
+                    }
+
+                }
+            }        
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lexical Analyzer/TokenAnalyzer.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexical_Analyzer
+{
+    class TokenAnalyzer
+    {
+        string _token;
+        char[] _dataArray;
+        string[] _operators = { "+", "-", "*", "/", "%", "&","(",")","[","]",
+            "|", "^", "!", "~", "&&", "||",",",
+            "++", "--", "<<", ">>", "==", "!=", "<", ">", "<=",
+            ">=", "=", "+=", "-=", "*=", "/=", "%=", "&=", "|=",
+            "^=", "<<=", ">>=", ".", "[]", "()", "?:", "=>", "??" };
+        string _specialChar = @"\|!#$%&/()=?»«*@£§€{}.-;'<>,";
+        string[] _keywords = { "abstract", "as", "base", "bool", "break", "by",
+            "byte", "case", "catch", "char", "checked", "class", "const",
+            "continue", "decimal", "default", "delegate", "do", "double",
+            "descending", "explicit", "event", "extern", "else", "enum",
+            "false", "finally", "fixed", "float", "for", "foreach", "from",
+            "goto", "group", "if", "implicit", "in", "int", "interface",
+            "internal", "into", "is", "lock", "long", "new", "null", "namespace",
+            "object", "operator", "out", "override", "orderby",  "params",
+            "private", "protected", "public", "readonly", "ref", "return",
+            "switch", "struct", "sbyte", "sealed", "short", "sizeof",
+            "stackalloc", "static", "string", "select",  "this",
+            "throw", "true", "try", "typeof", "uint", "ulong", "unchecked",
+            "unsafe", "ushort", "using", "var", "virtual", "volatile",
+            "void", "while", "where", "yield" };
+        string _qmark = "\"'";
+
+        public void input_check(string data)
+        {
+            int number;          
+            
+                if (data.All(char.IsDigit) == false)
+                {
+                    word_check(data);
+                }
+                else
+                {
+                    number = Convert.ToInt32(data);
+                    numerical_check(number);
+                }           
+        }
+
+        public void word_check(string data)
+        {
+            _dataArray = data.ToCharArray();
+
+            if (_keywords.Contains(data) == true)
+            {
+                _token = "Keyword";
+            }
+            else if((Array.IndexOf(_operators, data) > -1) == true)
+            {               
+                _token = "Symbol";
+            }
+            else if(_qmark.Contains(_dataArray[0]) == true  && _qmark.Contains(_dataArray[data.Length-1]) == true)
+            {
+                _token = "String/Character Literal";
+            }            
+            else
+            {
+                variable(data);
+            }
+
+            answer(data, _token);
+
+        }   
+      
+        public void numerical_check(int number)
+        {
+            _token = "Number";
+
+            answer(number.ToString(), _token);
+        }        
+
+        public void variable(string data)
+        {
+            StringBuilder newVariable = new StringBuilder();
+
+            _dataArray = data.ToCharArray();
+
+
+            if (char.IsDigit(_dataArray[0]) == false || _dataArray[0] == '_')
+            {
+                for(int i=0; i<data.Length; i++)
+                {
+                
+                    if (_specialChar.Contains(_dataArray[i]) == false)
+                    {
+                        newVariable.Append(_dataArray[i]);
+                    }
+                    else
+                    {
+                        _token = "Invalid Input!";
+                    }
+                }
+                
+                if(_token!= "Invalid Input")
+                {
+                    _token = "Variable";
+                }                
+            }
+            else
+            {
+                _token = "Invalid Input!";
+            }          
+        }
+
+        public void answer(string data, string _token)
+        {
+            Console.WriteLine(data + " = " + _token);
+        }
+    }
+}
